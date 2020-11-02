1baf8644ef70153707e24c54af004943c528ae96
Derek <derek.j.n.young@gmail.com>
2020-11-02 12:04:39 -0800

    Chipping a single image minimally operational

--- scripts/chip-image.R
+++ scripts/chip-image.R
@@ -1,4 +1,5 @@
 library(magick)
+library(tidyverse)
 
 img = image_read("data/sample.jpg")
 
@@ -13,13 +14,44 @@ height = info$height
 
 
 ### get number of horizontal and vertical chips (does not include the final rightmost and bottom-most chip in each dimension)
-n_h_ch = width / (chip_size*(overlap/100)) %>% floor
-n_v_ch = height / (chip_size*(overlap/100)) %>% floor
+window_step = (chip_size*(overlap/100))
+
+n_h_ch = floor(width / (chip_size*(overlap/100))) - 1 # subtract one because the final value less then a full chip-width from the edge of the image
+n_v_ch = floor(height / (chip_size*(overlap/100))) - 1
 
 ### get the coords of the upper left corner of each chip
-corners_x = 
+cols = 1:n_h_ch
+rows = 1:n_v_ch
+
+corners_x = (cols-1) * window_step
+corners_y = (rows-1) * window_step
+# add a final column to include the incomplete remainder
+corners_x = c(corners_x,(width-chip_size))
+corners_y = c(corners_y,(height-chip_size))
+
+tiles = expand_grid(x_coord=corners_x,y_coord=corners_y)
+tiles$colnum = rep(1:length(corners_x),each=(length(corners_y)))
+tiles$rownum = rep(1:length(corners_y),(length(corners_x)))
+tiles$tile_id = 1:nrow(tiles)
+
+## crop to each tile; write to file
+for(i in tiles$tile_id) {
   
-corners_y = 
+  tile_x = tiles[i,"x_coord"]
+  tile_y = tiles[i,"y_coord"]
+  
+  tile_colnum = tiles[i,"colnum"]
+  tile_rownum = tiles[i,"rownum"]
+  
+  crop_string = paste0(chip_size,"x",chip_size,"+",tile_x,"+",tile_y)
+  img_crop = image_crop(img,crop_string)
+  
+  # name for image is colnum-rownum
+  name = paste0("chip_",tile_colnum,"-",tile_rownum,".jpg")
+  
+  image_write(img_crop,paste0("data/chipped/",name),format="jpg")
+}
+
 
 
 
